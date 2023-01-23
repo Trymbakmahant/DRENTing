75547419d8868f2c0683156a69672e7dc3172396
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-23 11:55:28 +0530

    rentandbuy

---
+++ client/src/components/cards/Buyrentcard.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const Buyrentcard = () => {
+  const [posts, setPost] = useState([]);
+  const a = async() => {
+    try {
+      const { data } = await axios.get(`https://dummyjson.com/products`);
+      setPost(data);
+     
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // a();
+  useEffect(()=>{
+    a();
+  })
+
+
+  
+  return (
+    <>
+     {/* <button onClick={a}> click me</button> */}
+     <div className="grid col-span-2 lg:grid-cols-3 place-content-center  divide-y  gap-4">
+
+    
+      {posts &&
+        posts.products &&
+        posts.products.map((post) => (
+      
+      
+           <div className =" p-8 	">
+              <div className="card w-96 h-96 card-bordered  glass ">
+                <figure>
+                   <a href={post.images[0]}>
+                  <img src={post.images[0] }onClick={post.images[0]} className="rounded-xl"/>
+
+                   </a>
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{post.title}</h2>
+                  {/* <h2>DESCRIPTION!</h2> */}
+                  <p>{post.description}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn ">Rent Now</button>
+                    <button className="btn  ">Buy Now</button>
+                  </div>
+                </div>
+                </div>
+              </div>
+             
+     
+        ))}
+        </div>
+           
+            
+    </>
+  );
+};
+export default Buyrentcard;
--- client/src/components/routes/Path.js
+++ client/src/components/routes/Path.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
 
+=======
+import Buyrentcard from "../cards/Buyrentcard";
+>>>>>>> Stashed changes
 import Home from "../Pages/home";
 
 const Paths = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+<<<<<<< Updated upstream
+=======
+      <Route exact path="/buyrent" element={<Buyrentcard/>}/>
+>>>>>>> Stashed changes
     </Routes>
   );
 };
