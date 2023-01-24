58a99f397542d21f9dcdf3ed7ab220f3c6b77f86
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-24 21:48:19 +0530

    Create buyrentt.js

---
+++ client/src/components/Pages/buyrent/buyrentt.js
@@ -0,0 +1,17 @@
+const Buy=()=>{
+    return(
+        <>
+           <div className="card card-side bg-base-100 pt-40 shadow-xl">
+  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
+        </>
+    )
+}
+export default Buy;
