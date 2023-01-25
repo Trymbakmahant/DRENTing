35ac07a9fae2346ddf664235f12ca1dcf0abd14c
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-01-25 20:34:21 +0530

    id link added

--- client/src/components/cards/Buyrentcard.js
+++ client/src/components/cards/Buyrentcard.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 const Buyrentcard = () => {
   const [posts, setPost] = useState([]);
@@ -8,7 +9,6 @@ const Buyrentcard = () => {
         `http://localhost:8081/api/productData/all`
       );
       setPost(data);
-    
     } catch (err) {
       console.log(err);
     }
@@ -16,48 +16,43 @@ const Buyrentcard = () => {
 
   useEffect(() => {
     a();
-    
   }, []);
 
-  useEffect(()=>{
-  
-  },[posts])
-
+  useEffect(() => {}, [posts]);
+  const id = "myass";
   return (
     <>
       {/* <button onClick={a}> click me</button> */}
       <div className="grid col-span-2 lg:grid-cols-3 place-content-center  divide-y  gap-4">
         {posts &&
-       
           posts.map((post) => (
             <div className=" p-8 	">
-
               <div className="card w-96 h-96 card-bordered shadow-xl  glass ">
-              <a
-            
-              href="/buyrent/[_id]"
-            >
-            
-            
-                <figure>
-                  <a href={post.imageUrl}>
-                    <img
-                      src={post.imageUrl}
-                      onClick={post.imageUrl}
-                      className="rounded-xl"
-                    />
-                  </a>
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{post.name}</h2>
-                  {/* <h2>DESCRIPTION!</h2> */}
-                  <p>{post.description}</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn ">Rent Now</button>
-                    <button className="btn  ">Buy Now</button>
+                <Link
+                  style={{ marginRight: "20px" }}
+                  exact
+                  className="nav-link btn-ghost"
+                  to={"/buy/" + post._id}
+                >
+                  <figure>
+                    <a href={post.imageUrl}>
+                      <img
+                        src={post.imageUrl}
+                        onClick={post.imageUrl}
+                        className="rounded-xl"
+                      />
+                    </a>
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{post.name}</h2>
+                    {/* <h2>DESCRIPTION!</h2> */}
+                    <p>{post.description}</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn ">Rent Now</button>
+                      <button className="btn  ">Buy Now</button>
+                    </div>
                   </div>
-                </div>
-              </a>
+                </Link>
               </div>
             </div>
           ))}
