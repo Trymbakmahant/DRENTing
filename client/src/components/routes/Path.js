30866b1bb0d5a7296ee9249afa9a3ef260fd85e9
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-26 09:10:45 +0530

    Update Path.js

--- client/src/components/routes/Path.js
+++ client/src/components/routes/Path.js
@@ -5,13 +5,14 @@ import Upload from "../Pages/upload";
 import Home from "../Pages/home";
 import Buy from "../Pages/buyrent/buyrentt";
 
+
 const Paths = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/upload" element={<Upload />} />
       <Route exact path="/buyrent" element={<Buyrentcard />} />
-      <Route exact path="/buy" element={<Buy />} />
+      <Route exact path="/buy/:_id" element={<Buy/>} />
     </Routes>
   );
 };
