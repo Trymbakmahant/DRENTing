0777bed9ff982da7b119659f66db6fa21d87bc96
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-27 00:13:04 +0530

    Update Navbar.js

--- client/src/components/Pages/Navbar.js
+++ client/src/components/Pages/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar border-2 bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">DRENTing</a>
       </div>
