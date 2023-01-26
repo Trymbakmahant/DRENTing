29460b69e8492a2ed186c90133a3b9741fb54d89
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-26 09:10:41 +0530

    Update buyrentt.js

--- client/src/components/Pages/buyrent/buyrentt.js
+++ client/src/components/Pages/buyrent/buyrentt.js
@@ -1,17 +1,54 @@
-const Buy=()=>{
-    return(
-        <>
-           <div className="card card-side bg-base-100 pt-40 shadow-xl">
-  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-        </>
-    )
-}
+import { useContext, useEffect, useState } from "react";
+import { accContext } from "../../context/accountContext";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+const Buy = () => {
+  const[post, setPost] = useState([]);
+
+  const { _id } = useParams();
+
+  const b = async () => {
+    try {
+      const { data } = await axios.post("http://localhost:8081/api/Checkid", {
+        _id,
+      });
+      setPost(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  useEffect(() => {
+    b();
+  }, []);
+  return (
+    <>
+     <div className=" p-8 	">
+        { post[0]&&post[0]._id&& <div className="card w-96 h-96   glass ">
+                  
+                    <figure>
+                      
+                        <img
+                          src={post[0].imageUrl}
+                          
+                          
+                        />
+                    
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{post[0].name}</h2>
+                      {/* <h2>DESCRIPTION!</h2> */}
+                      <p>{post[0].description}</p>
+                      <div className="card-actions justify-end">
+                        <button className="btn ">Rent Now</button>
+                        <button className="btn  ">Buy Now</button>
+                      </div>
+                    </div>
+                
+                </div>}
+            </div>
+    </>
+  );
+};
 export default Buy;
