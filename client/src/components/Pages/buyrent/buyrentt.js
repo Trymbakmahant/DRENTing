b523f0b357d4a1d758b64f9d0b4521f7b4942424
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-28 17:43:43 +0530

    Update buyrentt.js

--- client/src/components/Pages/buyrent/buyrentt.js
+++ client/src/components/Pages/buyrent/buyrentt.js
@@ -7,20 +7,14 @@ const Buy = () => {
   const [owner, setOwner] = useState("");
   const [name, setName] = useState("");
   const [flowrate, setFlowrate] = useState("");
+  const [loading, setLoading] = useState(false);
   const { _id } = useParams();
+  
+    const enddateInputRef = useRef(null);
+    const [startdate, setStartDate] = useState("");
+    const [enddate, setEndDate] = useState("");
   const ctx = useContext(accContext);
   const startdateInputRef = useRef(null);
-  const enddateInputRef = useRef(null);
-  const [startdate, setStartDate] = useState("");
-  const [enddate, setEndDate] = useState("");
-
-  const handlestartChange = (e) => {
-    setStartDate(e.target.value);
-  };
-  const handleendChange = (e) => {
-    setEndDate(e.target.value);
-  };
-
   const b = async () => {
     try {
       const { data } = await axios.post("http://localhost:8081/api/Checkid", {
@@ -39,6 +33,8 @@ const Buy = () => {
   const productname = name;
 
   async function payment() {
+
+    setLoading(true);
     await ctx.dataState.createNewFlow(owner, flowrate);
     const dataofhistory = { flowrate, user, startdate, enddate, productname };
     const data = await axios.post(
@@ -46,7 +42,17 @@ const Buy = () => {
       dataofhistory
     );
     console.log(data);
+   
+   setLoading(false);
+   
   }
+
+  const handlestartChange = (e) => {
+    setStartDate(e.target.value);
+  };
+  const handleendChange = (e) => {
+    setEndDate(e.target.value);
+  };
 
   useEffect(() => {
     b();
@@ -55,8 +61,12 @@ const Buy = () => {
     <>
       <div className="flex p-8 justify-center pt-20	">
         {post[0] && post[0]._id && (
-          <div className="card card-side h-8/12	w-8/12 shadow-xl  ring ring-offset-4 glass ">
-            <figure>
+          
+            
+          <div className="card card-side h-8/12	w-8/12 shadow-xl  ring ring-offset-4 glass">
+            <figure >
+           
+            
               <img
                 className="	 w-[1000px] h-[700px] scale-75	 "
                 src={post[0].imageUrl}
@@ -72,46 +82,45 @@ const Buy = () => {
               <div className="font-mono text-xl">buy price: </div>
               {/* <button className="btn  ">Buy Now</button> */}
               <div> {post[0].buying}</div>
-              <div className="card-actions justify-end">
-                <a href="#my-modal-2" className="btn">
-                  rent now
-                </a>
-                <button className="btn  ">Buy Now</button>
-                <div className="modal" id="my-modal-2">
-                  <div className="modal-box">
-                    <div className="flex justify-between">
-                      <div>
-                              
-                        <input
-                          type="date"
-                          onChange={handlestartChange}
-                          ref={startdateInputRef}
-                        />
-                              <p className="pl-11">Start Date: {startdate}</p>
-                            
-                      </div>
-                      <div>
-                              
-                        <input
-                          type="date"
-                          onChange={handleendChange}
-                          ref={enddateInputRef}
-                        />
-                              <p className="pl-11">End Date: {enddate}</p>
-                            
-                      </div>
-                    </div>
-                    <p className="py-4">
-                      total money you have to pay for this ...
-                    </p>
-                    <div className="modal-action">
-                      <button onClick={payment}> make payment</button>
-                    </div>
-                  </div>
-                </div>
+
+               
+                <a href="#my-modal-2" className="btn btn-current outline">{loading? "Loading....":"rent now  "}</a>
+              <button className="btn btn-current outline "> buy now</button>
+                
+              
+                  <div className="modal " id="my-modal-2">
+                <div className="modal-box">
+                <div className="flex justify-between">
+                <div>
+                    <input
+                      type="date"
+                      onChange={handlestartChange}
+                      ref={startdateInputRef}
+                    />
+                    <p className="pl-11">Start Date: {startdate}</p>
+                  </div>
+                <div>
+                    <input
+                      type="date"
+                      onChange={handleendChange}
+                      ref={enddateInputRef}
+                    />
+                    <p className="pl-11">End Date: {enddate}</p>
+                  </div>
+              </div>
+    <p className="py-4">total money you have to pay for this ...</p>
+    <div className="modal-action">
+     <a href="#" className="btn" onClick={payment}>
+      make payment</a>
+    </div>
+  </div>
+</div>
+             
+            
+
               </div>
             </div>
-          </div>
+         
         )}
       </div>
     </>
