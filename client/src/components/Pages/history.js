8a6c82faa93a1b6249eef0b670f58bae0c025af5
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-01-29 16:03:39 +0530

    Update history.js

--- client/src/components/Pages/history.js
+++ client/src/components/Pages/history.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import { accContext } from "../context/accountContext";
+
 function History() {
   const data = { adress: "" };
   const ctx = useContext(accContext);
+  const [histry,setHistry]= useState([]);
   data.adress = ctx.dataState.acclogin.accountAddress;
   console.log(data);
   const callfunction = async () => {
@@ -11,16 +13,28 @@ function History() {
       "http://localhost:8081/api/history/get",
       data
     );
-
-    console.log(datais.data);
+    setHistry(datais.data)
+    console.log(histry);
   };
   useEffect(() => {
     callfunction();
-  }, []);
+
+  }, [data.adress]);
   return (
-    <div>
-      hello world
-      <h1>kya kr ra h bhai</h1>
+    <div className=" flex pt-10 justify-center">
+
+{  histry.map((data)=>(<div className="card w-96 bg-base-100  ring-2 ring-white shadow-xl">
+<div className="card-body">
+    <h2 className="card-title"> Productname : {data.productname}</h2>
+    <p> Flowreate : {data.flowrate}</p>
+    <p> user : {data.user}</p>
+    <p className="btn btn-primary">start date {data.startdate}</p>
+    <p className="btn btn-primary">end date {data.enddate}</p>
+    <div className="card-actions justify-end">
+    
+    </div>
+</div>
+</div>))}
     </div>
   );
 }
